8ec9f89dd7dd8d76b9055bdbdf7e97862e671fea
gqroga <gcqueirogat@gmail.com>
2025-03-03 05:00:11 -0300

    first commit

---
+++ src/main/java/com/med/voll/api/ApiVollmedApplication.java
@@ -0,0 +1,13 @@
+package com.med.voll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVollmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVollmedApplication.class, args);
+	}
+
+}
