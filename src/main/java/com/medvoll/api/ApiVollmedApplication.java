3326fa26d1fef8e5af2ac621117c523703bfab17
gqroga <gcqueirogat@gmail.com>
2025-03-10 19:43:40 -0300

    first commit

---
+++ src/main/java/com/medvoll/api/ApiVollmedApplication.java
@@ -0,0 +1,13 @@
+package com.medvoll.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVollmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVollmedApplication.class, args);
+	}
+
+}
